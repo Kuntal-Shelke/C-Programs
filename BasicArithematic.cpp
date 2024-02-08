bfb905a104f1fe6b9a3a6c51598b722a859fded6
Kuntal-Shelke <156524938+Kuntal-Shelke@users.noreply.github.com>
2024-02-08 20:07:04 +0530

    Add files via upload

---
+++ BasicArithematic.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int A,B;
+	cin>>A>>B;
+	cout<<"Addition = "<<A+B<<endl;
+	cout<<"Subtraction ="<<A-B<<endl;
+	cout<<"Multiplication = "<<A*B<<endl;
+	cout<<"Division ="<<A/B<<endl;
+	cout<<"Mod = "<<A%B;
+
+	return 0;
+}
